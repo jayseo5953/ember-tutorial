f5fc16ce54da30f3965ce61b14d8f43967a6a65d
Jay Seo <sjs5953@gmail.com>
2022-05-30 23:49:00 -0700

    end of tutorial

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,8 @@ module.exports = function (environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN =
+    'pk.eyJ1Ijoic2pzNTk1MyIsImEiOiJjbDNwb3h6NDUwejRxM2JwZWd2ZTN5ajduIn0.aKZTYBX6HvwSYt9G9mT2Ng';
+
   return ENV;
 };
